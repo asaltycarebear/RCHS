649c9518b6c06c7c010bf14686a4b733d357c4f6
asaltycarebear <garbear.stockwell@gmail.com>
2025-08-18 13:43:48 -0600

    Menu Update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.querySelector(`#menu`); /*# target id's*/
+    const navMenu = document.querySelector(`#nav-menu`);
+
+    menuButton.addEventListener("click", () => { /*=> is used in place of function ()*/
+        navMenu.classList.toggle(`open`);
+
+        /*Toggle*/ 
+        if (navMenu.classList.contains(`open`)) {
+            menuButton.textContent = `✖`;
+        } 
+        else {
+            menuButton.textContent = `☰`;
+        }
+    });
+});
+
 // Handle form submission on index.html
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
